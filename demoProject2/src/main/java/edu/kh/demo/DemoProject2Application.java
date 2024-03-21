a5706cb6a0273c1a40598aacf87e5529f125dfe6
khs <hanseog201@naver.com>
2024-03-21 15:41:35 +0900

    수업종료

--- demoProject2/src/main/java/edu/kh/demo/DemoProject2Application.java
+++ demoProject2/src/main/java/edu/kh/demo/DemoProject2Application.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoProject1Application {
+public class DemoProject2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoProject1Application.class, args);
-		
+		SpringApplication.run(DemoProject2Application.class, args);
 	}
 
 }
